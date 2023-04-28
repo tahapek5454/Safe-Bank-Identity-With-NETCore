3d4e7f81e6b583b2b6eb0a96352e338dca97eb04
tahapek5454 <tahapek5454@gmail.com>
2023-04-29 00:02:29 +0300

    build: build arch

---
+++ SafeBankIdentity.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SafeBankIdentity.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
