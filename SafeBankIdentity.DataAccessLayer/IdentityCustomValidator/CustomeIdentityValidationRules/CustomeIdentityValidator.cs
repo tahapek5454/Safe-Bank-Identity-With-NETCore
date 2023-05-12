146978ab8d87d8d29cf37f86ea33159c7ac97908
tahapek5454 <tahapek5454@gmail.com>
2023-05-12 14:52:41 +0300

    feat: add custom validation rule for identity

---
+++ SafeBankIdentity.DataAccessLayer/IdentityCustomValidator/CustomeIdentityValidationRules/CustomeIdentityValidator.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeBankIdentity.DataAccessLayer.IdentityCustomValidator.CustomeIdentityValidationRules
+{
+	public class CustomeIdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = nameof(PasswordTooShort),
+				Description = $"Şifre uzunluğu minumum {length} karakter olmalıdır."
+			};
+		}
+
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = nameof(PasswordRequiresUpper),
+				Description = "En az bir büyük harf olmalıdır."
+			};
+		}
+
+		public override IdentityError PasswordRequiresLower()
+		{
+			return new IdentityError()
+			{
+				Code = nameof(PasswordRequiresLower),
+				Description = "En az bir küçük harf olmalıdır."
+			};
+		}
+
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = nameof(PasswordRequiresDigit),
+				Description = "En az bir rakam olmalıdır."
+			};
+		}
+
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = nameof(PasswordRequiresNonAlphanumeric),
+				Description = "En az bir sembol olmalıdır."
+			};
+		}
+	}
+}
--- SafeBankIdentity.DataAccessLayer/ServiceRegistration.cs
+++ SafeBankIdentity.DataAccessLayer/ServiceRegistration.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SafeBankIdentity.DataAccessLayer.Abstract;
 using SafeBankIdentity.DataAccessLayer.Concrete;
+using SafeBankIdentity.DataAccessLayer.IdentityCustomValidator.CustomeIdentityValidationRules;
 using SafeBankIdentity.DataAccessLayer.Repositories;
 using SafeBankIdentity.EntityLayer.Concrete;
 using System;
@@ -14,13 +15,14 @@ using System.Threading.Tasks;
 
 namespace SafeBankIdentity.DataAccessLayer
 {
-    public static class ServiceRegistration
+	public static class ServiceRegistration
     {
         public static void AddDataAccessRegistration(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
             serviceCollection.AddDbContext<Context>(options => options.UseNpgsql(configuration["ConnectionStrings:PostgreSQL"]));
             serviceCollection.AddIdentity<AppUser, AppRole>()
                 .AddEntityFrameworkStores<Context>()
+                .AddErrorDescriber<CustomeIdentityValidator>()
                 .AddDefaultTokenProviders();
 
             serviceCollection.AddScoped<ICustomerAccountDal, CusomerAccountDal>();
