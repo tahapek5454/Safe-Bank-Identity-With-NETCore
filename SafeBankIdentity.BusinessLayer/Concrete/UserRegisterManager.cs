7df0aa3d592d5a95121783cd00248dc643e7f629
tahapek5454 <tahapek5454@gmail.com>
2023-05-16 17:05:07 +0300

    feat: add confirmCode column

--- SafeBankIdentity.BusinessLayer/Concrete/UserRegisterManager.cs
+++ SafeBankIdentity.BusinessLayer/Concrete/UserRegisterManager.cs
@@ -21,12 +21,14 @@ namespace SafeBankIdentity.BusinessLayer.Concrete
 
         public async Task<IdentityResult> RegisterAsync(AppUserRegisterDto appUserRegisterDto)
         {
+            Random random= new Random();
             AppUser appUser = new AppUser()
             {
                 UserName = appUserRegisterDto.UserName,
                 Name = appUserRegisterDto.Name,
                 Email = appUserRegisterDto.Email,
                 Surname = appUserRegisterDto.Surname,
+                ConfirmCode = random.Next(100000, 1000000)
             };
 
             IdentityResult identityResult = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
